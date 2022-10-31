6ab180c5c56812167de898219ff84fb750281477
Gregory <degeemon@gmail.com>
2022-10-31 17:04:11 +0800

    add Where Anki media folder located

--- anki_generator_gui.py
+++ anki_generator_gui.py
@@ -17,14 +17,15 @@ def generate_anki_deck_import(filelist):
 
 layout = [
     [sg.Text('ANKI GENERATOR 0.0.1 - by grego')], 
-    [sg.Text("Select Folder: Folder must contain even images:")],
+    [sg.Text("(1) Select Folder (Folder must contain even number of images):")],
     [sg.In(size=(70,1), enable_events=True ,key='-FOLDER-'), sg.FolderBrowse()],
-    [sg.Text("Output pairings:")],
+    [sg.Text("(2) Review the output pairings:")],
     [sg.Listbox(values=[], enable_events=True, horizontal_scroll=True, size=(80,10),key='-FILE LIST-')],
-    [sg.Text("Output File:")],
+    [sg.Text("(3) Output File:")],
     [sg.In(size=(68,1), enable_events=True ,key='-OUTPUT-'), sg.FileSaveAs()],
-    [sg.Text("Generate Anki CSV for Import:")],
-    [sg.Button("Generate",key='-GENERATE-'), sg.Button("Open Anki Media Folder", key='-OPEN-MEDIA-FOLDER-')]
+    [sg.Text("(4) Generate Anki CSV for Import:"), sg.Button("Generate",key='-GENERATE-')],
+    [sg.Text("(5) Paste your images to Anki's collection.media folder"), 
+            sg.Button("Where is Anki Media Folder?", key='-OPEN-MEDIA-FOLDER-')]
 ]
 
 
@@ -42,8 +43,17 @@ while True:
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
     if event == '-OPEN-MEDIA-FOLDER-':
-        sg.popup(f"Feature Not ready yet",
-                    keep_on_top=True)
+        media_folder_text = """
+            Refer to:                                                                          
+            https://docs.ankiweb.net/files.html#file-locations
+            
+            Windows: "%APPDATA%\\Anki2" in your File Manager's Location Field
+            Mac: ~/Library/Application Support/Anki2
+            Linux: ~/.local/share/Anki2 /
+            Linux(Flatpak): ~/.var/app/net.ankiweb.Anki/data/Anki2/Main/collection.media
+
+            """
+        sg.popup(media_folder_text, line_width=100, keep_on_top=True)
         # win_anki_dir = os.getenv('APPDATA')
         # path = os.path.realpath(win_anki_dir)
         # os.system(f"start {os.path.realpath(path)}")
